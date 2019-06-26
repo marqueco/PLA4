7ce1a43aa9a603cae0cae2fe2681d27dec6b32bb
Maria Arquer <Maria Arquer@DESKTOP-IDHKT9P.Home>
2019-06-26 18:00:02 +0200

    feina de dia 25

--- pla4/scriptsFULL2.js
+++ pla4/scriptsFULL2.js
@@ -16,19 +16,57 @@ function full2Exercici1() {
 	return comptador
 }
 
-function full2Exercici2() {
-		var abecedari = ["a", "b", "c", "ç", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-		var cadenaCar = "lkjndfiobisaospañnjb";
-		var llistaCars = [];
-		for (var posicio = 0; posicio <= cadenaCar.length; posicio++) {
-			for (var comptador = 0; comptador < abecedari.length; comptador++) {	
-			if (((cadenaCar.charAt(posicio)) == abecedari[comptador])) {
-				llistaCars.push(comptador);
-				console.log(comptador);
+function full2Exercici2 () {
+	var abecedari = ["a", "b", "c", "ç", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	var cadenaCar = "lkjndfiobisaospañnjb";
+	var llistaCars = [];
+	for (var indexLletraBuscada = 0; indexLletraBuscada < abecedari.length; indexLletraBuscada++) {
+		var comptador = 0;
+			for (var posicio = 0; posicio < cadenaCar.length; posicio++) {	
+			if (((cadenaCar[posicio]) == (abecedari[indexLletraBuscada]))) {
+				comptador++;
 			}		
 			}
 	llistaCars.push(comptador);
-			console.log(comptador);
 		}
 		return llistaCars;
 		}
+
+
+
+
+function full2Exercici3 () {
+	var llista1 = [ Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10) ];
+	var llista2 = [ Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10) ];
+	var llista3 = [ Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10) ];
+	var llista4 = [ Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10) ];
+	var llista5 = [ Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10),
+		Math.floor(Math.random() * 10), Math.floor(Math.random() * 10) ];
+	var llistes = [].concat(llista1, llista2, llista3, llista4, llista5);
+	var sumaDiagonal = (llista1[0] + llista2[1] + llista3[2] + llista4[3] + llista5[4]);
+	console.log(llista1);
+	console.log(llista2);
+	console.log(llista3);
+	console.log(llista4);
+	console.log(llista5);
+	console.log(sumaDiagonal);
+return sumaDiagonal
+}
+
+function full2Exercici4 () {
+	var matrix = [a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30,a31,a32,a33,a34,a35];
+	var vector1.length = 5;
+	var vector2.length = 5;
+	var vector3.length = 5;
+	var vector4.length = 5;
+	var vector5.length = 5;
+}
