6ad5f34115d6aeb4952329935ea7f31999cf2e79
Maria Arquer <Maria Arquer@192.168.1.130>
2019-06-28 17:14:13 +0200

    He refet l'exercici 6 del full 2.

--- pla4/scriptFull2Exercici6.js
+++ pla4/scriptFull2Exercici6.js
@@ -11,19 +11,19 @@ function sumaDiagonal(inputVector) {
 	var n = (Math.sqrt(len));
 	var casella = 0;
 	var suma = 0;
-	if (n === parseInt(n, 10)) {
-		for (j = 1; j < n; j++){
-			for (i = j; i < n; i++);
-			if (inputVector[j][i]) = 0 {
-				return 
-			} else if {(inputVector[j][i]) < 0 
-				
-			} else if { (inputVector[j][i]) > 0 
-							
-			}	
-			return "TRUE" + "<br>" + "Treballem sobre una matriu de " + n + "*" + n +". El resultat de la suma dels elements sobre la diagonal es " + suma + "."
-		 }  else {
-		    return "FALSE" + "<br>" + "La matriu que has proporcionat no es quadrada. Proporciona una matriu de n*n.";
+		if (n === parseInt(n, 10)) {
+			for (var i = 0; i < n; i++) {
+				suma += inputVector[casella];
+				console.log(suma);
+				console.log(casella);
+				casella = casella + n + 1;
+			}				
+	
+		return "TRUE" + "<br>" + "Treballem sobre una matriu de " + n + "*" + n +". El resultat de la suma dels elements sobre la diagonal de la matriu es " + suma + "."
+	
+		} else {
+	
+		return "FALSE" + "<br>" + "La matriu que has proporcionat no es quadrada. Proporciona una matriu de n*n.";
 		    console.log(n);
 		 } 
 	}
