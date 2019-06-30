0d39a3efa2109302c3c82ad0dff3898bf6cebc2a
Maria Arquer <Maria Arquer@DESKTOP-IDHKT9P>
2019-06-30 18:51:01 +0200

    he acabat la primera PLAAAAAAAAA

--- pla4/scriptFull2Exercici7.js
+++ pla4/scriptFull2Exercici7.js
@@ -9,31 +9,35 @@ function triangleSuperior(matriu) {
 	var comptadorMajors = 0;
 	var comptadorMenors = 0;
 	var triangle = [];
-	var matriu = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]];
+	var matriu = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,0],[16,17,18,19,-20],[21,22,23,24,25]];
 	
-	for (rowNum = 1; rowNum < n; rowNum++) {
-		var valorCasella = [];
-		for (colNum = 0; colNum < n; colNum++) {
-			valorCasella.push(matriu[colNum][rowNum]);
+	for (colNum = 0; colNum < n; colNum++) {
+		//var valorCasella = [];
+		for (rowNum = 0; rowNum < colNum; rowNum++) {
+			//valorCasella.push(matriu[colNum][rowNum]);
 			
-			console.log(colNum);
-			console.log(rowNum);
-			console.log(valorCasella);
-		
-			if (matriu[colNum][rowNum] > 0) {
+			console.log(matriu[rowNum][colNum]);
+			//console.log(rowNum);
+			//console.log(valorCasella);
+			
+			
+			if (matriu[rowNum][colNum] > 0) {
 				comptadorMajors++;
 				
 				
-				} else if (matriu[colNum][rowNum] == 0) {
+				} else if (matriu[rowNum][colNum] == 0) {
 				comptadorZeros++;
 					
-				} else { (matriu[colNum][rowNum] < 0)
+				} else {
 				comptadorMenors++;
+		
+		
 				}
-			
-			rowNum += 1;
-			colNum += rowNum + 1;
-}
+	
+		}
 	}
+		console.log(comptadorZeros);
+		console.log(comptadorMajors);
+		console.log(comptadorMenors);
 return "hola"
 }
